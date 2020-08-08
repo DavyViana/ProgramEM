bb67b09f6669d52612a8def5bbe0d0b13dab8f84
marcusportela <marcustalegal.mp@gmail.com>
2020-08-08 16:41:40 -0300

    Exclusão de pastas

--- src/main/java/com/programem/site/securitiInit.java
+++
@@ -1,6 +0,0 @@
-package com.programem.site;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class securitiInit extends AbstractSecurityWebApplicationInitializer {
-    
-}
