f9136e227e28f97259cd09f55bb82a686f514d92
marcusportela <marcustalegal.mp@gmail.com>
2020-08-08 14:53:18 -0300

    Clean code in object

--- src/main/java/com/programem/site/Inscricao.java
+++ src/main/java/com/programem/site/Inscricao.java
@@ -5,12 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity /** indica que o Banco de Dados deve criar uma tabela para esse objeto */
+@Entity /* BANCO DE DADOS CIRANDO O OBJETO */
 public class Inscricao {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    
-    /**         Informações gerais               */
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    /* INFORMAÇÃO GERAIS */
     private Integer id;
     private String nome;
     private String email;
@@ -18,184 +18,170 @@ public class Inscricao {
     private String estado;
     private String cidade;
     private Integer numero;
+    /* INFORMAÇÃO PESSOAIS */
+    /* INDENTIDADE DE GENERO */
+    private String identidadeDeGenero;
+    private String orientacaoSexual;
+    private String identidadeEtnico;
+    private String modalidade;
+    private String ano;
+    /* PROGAMAÇÃO */
+    private String interesseProgramacao;
+    private String estudoPrevio;
+    private String disponibilidade;
+    private String interesseCurso;
 
-    /**         Informações pessoais              */
-    /**         Identidade de gênero              */
-
-    String identidadeDeGenero;
-
-    /**         Orientação sexual                 */
-
-    String orientacaoSexual;
-
-    /**         Identidade etnico-racial           */
-
-    String identidadeEtnico;
-
-    /**         Escolaridade                       */
-
-    String modalidade;
-
-    String ano;
-
-     /**         Programação                       */
-
-    String interesseProgramacao;
-
-    String estudoPrevio;
-
-    String disponibilidade;
-
-    String interesseCurso;
-    
-    /*SET INFO GERAL*/
-
-    public void setId(Integer id){
+    /* SET INFO GERAL */
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setDatadenascimento(String dataDeNascimento){
+    public void setDatadenascimento(String dataDeNascimento) {
         this.dataDeNascimento = dataDeNascimento;
     }
 
-    public void setEstado(String estado){
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    public void setCidade(String cidade){
+    public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
-    public void setNumero(Integer numero){
+    public void setNumero(Integer numero) {
         this.numero = numero;
     }
 
-    /*SET INFO PESSOAL*/
-    /*IDENTIDADE DE GENERO*/
+    /* SET INFO PESSOAL */
+    /* IDENTIDADE DE GENERO */
 
-    public void setIdentidadeDeGenero(String identidadeDeGenero){
+    public void setIdentidadeDeGenero(String identidadeDeGenero) {
         this.identidadeDeGenero = identidadeDeGenero;
     }
 
-    /*ORIENTAÇÃO SEXUAL*/
+    /* ORIENTAÇÃO SEXUAL */
 
-    public void setOrientacaoSexual(String orientacaoSexual){
+    public void setOrientacaoSexual(String orientacaoSexual) {
         this.orientacaoSexual = orientacaoSexual;
     }
 
-    /*IDENTIDADE ETNICO*/
-    
-    public void setIdentidadeEtnico(String identidadeEtnico){
+    /* IDENTIDADE ETNICO */
+
+    public void setIdentidadeEtnico(String identidadeEtnico) {
         this.identidadeEtnico = identidadeEtnico;
     }
 
-    /*ESCOLARIDADE*/
+    /* ESCOLARIDADE */
 
-    public void setModalidade(String modalidade){
+    public void setModalidade(String modalidade) {
         this.modalidade = modalidade;
     }
 
-    public void setAno(String ano){
+    public void setAno(String ano) {
         this.ano = ano;
     }
 
-    /*PROGRAMAÇÃO*/
+    /* PROGRAMAÇÃO */
 
-    public void setInteresseProgramacao(String interesseProgramacao){
+    public void setInteresseProgramacao(String interesseProgramacao) {
         this.interesseProgramacao = interesseProgramacao;
     }
 
-    public void setEstudoPrevio(String estudoPrevio){
+    public void setEstudoPrevio(String estudoPrevio) {
         this.estudoPrevio = estudoPrevio;
     }
 
-    public void setDisponibilidade(String disponibilidade){
+    public void setDisponibilidade(String disponibilidade) {
         this.disponibilidade = disponibilidade;
     }
 
-    public void setInteressecurso(String interesseCurso){
+    public void setInteressecurso(String interesseCurso) {
         this.interesseCurso = interesseCurso;
     }
-    /*INICIO GET*/
-    /*INFORMAÇÃO GERAL*/
-    public Integer getId(){
+
+    /* INICIO GET */
+    /* INFORMAÇÃO GERAL */
+    public Integer getId() {
         return this.id;
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
-    public String getEmail(){
+
+    public String getEmail() {
         return this.email;
     }
-    public String getDatadenascimento(){
+
+    public String getDatadenascimento() {
         return this.dataDeNascimento;
     }
 
-    public String getEstado(){
+    public String getEstado() {
         return this.estado;
     }
 
-    public String getCidade(){
+    public String getCidade() {
         return this.cidade;
     }
-    
-    public Integer getNumero(){
+
+    public Integer getNumero() {
         return this.numero;
     }
-    /**Informações Pessoais         */
-    /**Identidade de genero         */
-    
-    public String getIdentidadedegenero(){
+
+    /** Informações Pessoais */
+    /** Identidade de genero */
+
+    public String getIdentidadedegenero() {
         return this.identidadeDeGenero;
     }
 
-    /**Identidade de sexual         */
+    /** Identidade de sexual */
 
-    public String getOrientacaosexual(){
+    public String getOrientacaosexual() {
         return this.orientacaoSexual;
     }
 
-    /**Identidade etnico-racial      */
+    /** Identidade etnico-racial */
 
-    public String getIdentidadeetnico(){
+    public String getIdentidadeetnico() {
         return this.identidadeEtnico;
     }
 
-    /**            Escolaridade      */
+    /** Escolaridade */
 
-    public String getModalidade(){
+    public String getModalidade() {
         return this.modalidade;
     }
-    public String getAno(){
+
+    public String getAno() {
         return this.ano;
     }
 
-     /**           Programação      */
+    /** Programação */
 
-     public String getInteresseprogramacao(){
+    public String getInteresseprogramacao() {
         return this.interesseProgramacao;
     }
 
-    public String getEstudoprevio(){
+    public String getEstudoprevio() {
         return this.estudoPrevio;
     }
 
-    public String getDisponibildade(){
+    public String getDisponibildade() {
         return this.disponibilidade;
     }
 
-    public String getInteressecurso(){
+    public String getInteressecurso() {
         return this.interesseCurso;
     }
-
 
 }
