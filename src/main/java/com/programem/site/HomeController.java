77ef94412ad0fbb60be5b0b6365ca0d30f20066f
marcusportela <marcustalegal.mp@gmail.com>
2020-08-09 22:40:04 -0300

    Tirando inscrição

--- src/main/java/com/programem/site/HomeController.java
+++ src/main/java/com/programem/site/HomeController.java
@@ -2,11 +2,11 @@ package com.programem.site;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    
+  @GetMapping("/")
     public String home(){
       return "home";
     }
